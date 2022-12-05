cffe40061e1e84188d708319c9be049360eee996
CrystalPharma <crystalpharmauk@gmail.com>
2022-12-05 19:09:10 +0000

    JS add event listener and create empty start game function

---
+++ questions.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question: 'What data type should be stored or be returned in a if statement ideally?',
+        answers: [
+            { text: 'Integers', correct: false},
+            { text: 'Boolean', correct: true},
+            { text: 'String', correct: false},
+            { text: 'Floats', correct: false},
+        ]
+    },
+    {
+        question: 'What data type should be stored or be returned in a if statement ideally?',
+        answers: [
+            { text: 'Integers', correct: false},
+            { text: 'Boolean', correct: true},
+            { text: 'String', correct: false},
+            { text: 'Floats', correct: false},
+        ]
+    },
+    {
+        question: 'What data type should be stored or be returned in a if statement ideally?',
+        answers: [
+            { text: 'Integers', correct: false},
+            { text: 'Boolean', correct: true},
+            { text: 'String', correct: false},
+            { text: 'Floats', correct: false},
+        ]
+    },
+    {
+        question: 'What data type should be stored or be returned in a if statement ideally?',
+        answers: [
+            { text: 'Integers', correct: false},
+            { text: 'Boolean', correct: true},
+            { text: 'String', correct: false},
+            { text: 'Floats', correct: false},
+        ]
+    }
+]
