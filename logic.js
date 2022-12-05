83802773d66402298fa64029a2b5dbd55772c02f
CrystalPharma <crystalpharmauk@gmail.com>
2022-12-05 19:19:41 +0000

    start Game ()

--- logic.js
+++ logic.js
@@ -1,7 +1,12 @@
 const startButton = document.getElementById ('start-button');
+const questionContain = document.getElementById ('questions');
 startButton.addEventListener ('click', startGame) 
 
 function startGame(){
-
+    startButton.classList.add ('hide');
+    // console.log("start game!");
+    var gameIntro = document.getElementById('start-screen');
+    gameIntro.classList.add('hide');
+    questionContain.classList.remove('hide');
 }
 
