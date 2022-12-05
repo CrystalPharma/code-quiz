3cc7c287df8e18aeb3f593a0bfee3c34035491ca
CrystalPharma <crystalpharmauk@gmail.com>
2022-12-05 22:11:12 +0000

    modified next question function

--- logic.js
+++ logic.js
@@ -2,11 +2,26 @@ const startButton = document.getElementById ('start-button');
 const questionContain = document.getElementById ('questions');
 startButton.addEventListener ('click', startGame) 
 
+
 function startGame(){
     startButton.classList.add ('hide');
     // console.log("start game!");
     var gameIntro = document.getElementById('start-screen');
     gameIntro.classList.add('hide');
     questionContain.classList.remove('hide');
+    nextQuestion();
 }
 
+let shuffledQuestion, currentQuestion;
+
+function nextQuestion(){
+    revealQuestion(shuffledQuestion[currentQuestion]);
+}
+
+function revealQuestion(){
+
+}
+
+function makeChoices(){
+
+}
