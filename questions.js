dc88ca4d0bcc0328366733813d770b1cd38dbbd0
CrystalPharma <crystalpharmauk@gmail.com>
2022-12-06 21:41:06 +0000

    change third question

--- questions.js
+++ questions.js
@@ -18,25 +18,16 @@ const questions = [
         ]
     },
     {
-        question: 'Som',
+        question: 'Inside which of the following HTML element should we put the JavaScript?',
         answers: [
-            { text: 'Integers', correct: false},
-            { text: 'Boolean', correct: false},
-            { text: 'String', correct: false},
-            { text: 'all of the above', correct: true},
+            { text: '<script>', correct: true},
+            { text: '<src>', correct: false},
+            { text: '<js>', correct: false},
+            { text: 'none of the above', correct: false},
         ]
     },
 //     {
 //         question: 'What is 22 -2 ?',
-//         answers: [
-//             { text: 'Integers', correct: false},
-//             { text: 'Boolean', correct: true},
-//             { text: 'String', correct: false},
-//             { text: 'Floats', correct: false},
-//         ]
-//     },
-//     {
-//         question: 'What data type should be stored or be returned in a if statement ideally?',
 //         answers: [
 //             { text: 'Integers', correct: false},
 //             { text: 'Boolean', correct: true},
