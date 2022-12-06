35a9411455bbfbed865c18e362f36831bf2c40bb
CrystalPharma <crystalpharmauk@gmail.com>
2022-12-06 10:09:12 +0000

    selectAnswer (e) update

--- logic.js
+++ logic.js
@@ -32,18 +32,28 @@ function revealQuestion(question) {
     button.innerText = answer.text;
     button.classList.add('button')
     if (answer.correct) {
-        button.dataset.correct = answer.correct
+        button.dataset.correct = answer.correct;
     }
     button.addEventListener('click', selectAnswer)
     answerButtonsElement.appendChild(button)
     })
 }
 
-function resetDefault() {
-    
+function clearAnswerButton() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild){
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
 }
 
-function makeChoices() {
-
+function selectAnswer(e) {
+const selectButton = e.target
+const correct = selectedButton.dataset.correct
+setClassState(document.body, correct)
+Array.from(answerButtonsElement.children).forEach(button => {
+setClassState(button, button.dataset.correct)
+})
+if (shuffledQuestion.length > currentQuestion +1) {nextButton.classList.remove('hide')};
 }
 
