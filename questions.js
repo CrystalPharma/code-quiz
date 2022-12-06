8d02234771ea97962cefbab0a1cf313bbb638142
CrystalPharma <crystalpharmauk@gmail.com>
2022-12-06 22:04:23 +0000

    push new questions

--- questions.js
+++ questions.js
@@ -2,37 +2,37 @@ const questions = [
     {
         question: 'What data type should be stored or be returned in a if statement ideally?',
         answers: [
-            { text: 'Integers', correct: false},
-            { text: 'Boolean', correct: true},
-            { text: 'String', correct: false},
-            { text: 'Floats', correct: false},
+            { text: 'Integers', correct: false },
+            { text: 'Boolean', correct: true },
+            { text: 'String', correct: false },
+            { text: 'Floats', correct: false },
         ]
     },
     {
         question: 'Array can be used to store __?',
         answers: [
-            { text: 'Integers', correct: false},
-            { text: 'Boolean', correct: false},
-            { text: 'String', correct: false},
-            { text: 'all of the above', correct: true},
+            { text: 'Integers', correct: false },
+            { text: 'Boolean', correct: false },
+            { text: 'String', correct: false },
+            { text: 'all of the above', correct: true },
         ]
     },
     {
         question: 'Inside which of the following HTML element should we put the JavaScript?',
         answers: [
-            { text: '<script>', correct: true},
-            { text: '<src>', correct: false},
-            { text: '<js>', correct: false},
-            { text: 'none of the above', correct: false},
+            { text: '<script>', correct: true },
+            { text: '<src>', correct: false },
+            { text: '<js>', correct: false },
+            { text: 'none of the above', correct: false },
         ]
     },
-//     {
-//         question: 'What is 22 -2 ?',
-//         answers: [
-//             { text: 'Integers', correct: false},
-//             { text: 'Boolean', correct: true},
-//             { text: 'String', correct: false},
-//             { text: 'Floats', correct: false},
-//         ]
-//     }
+    {
+        question: 'What is the correct syntax for changing the following HTML element? <p id = demo> demo </p>',
+        answers: [
+            { text: 'document.getElementbyTag' , correct: false },
+            { text: 'demo.innerHTML', correct: false },
+            { text: 'document.getElementById("demo").innerHTML', correct: true },
+            { text: 'document.getElementByName("demo").innerHTML', correct: false },
+        ]
+    }
 ]
